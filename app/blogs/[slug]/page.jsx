6542a7994e64bd41fc9cca5c144d029eed500ace
Ricardo Gomez <ricardogomezayala@gmail.com>
2025-12-28 12:18:27 -0500

    Arreglamos la pagina del articulo del blog V18

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -7,34 +7,24 @@ export async function generateMetadata({ params }) {
 
   if (!slug) {
     return {
-      title: "Artículo no encontrado",
+      title: "Artículo no encontrado SEO",
       robots: { index: false, follow: false },
     };
   }
 
-  const postSEO = await getPostSEO(params.slug);
+  const post = await getPostSEO(slug);
+  if (!post) return { title: "Artículo no encontrado SEO", robots: { index: false, follow: false } };
 
-  if (!postSEO) {
-    return {
-      title: "Artículo no encontrado",
-      description: "El contenido no está disponible",
-      robots: {
-        index: false,
-        follow: false,
-      },
-    };
-  }
-
-  const seo = postSEO.seo || {};
+  const seo = post.seo || {};
 
   return {
-    title: seo.title || postSEO.title,
+    title: seo.title || post.title,
     description: seo.metaDesc || "",
     alternates: {
       canonical: seo.canonical,
     },
     openGraph: {
-      title: seo.opengraphTitle || postSEO.title,
+      title: seo.opengraphTitle || post.title,
       description: seo.opengraphDescription || "",
       url: seo.canonical,
       type: "article",
@@ -47,64 +37,18 @@ export async function generateMetadata({ params }) {
 
 
 
-
-async function fetchPostBySlug(slug) {
-
-  if (!slug) return null;
-
-  const res = await fetch(process.env.WP_GRAPHQL_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: `
-        query PostBySlug($slug: ID!) {
-          post(id: $slug, idType: SLUG) {
-            title
-            content
-            date
-            featuredImage {
-              node {
-                sourceUrl
-                altText
-              }
-            }
-            author {
-              node {
-                name
-              }
-            }
-          }
-        }
-      `,
-      variables: { slug },
-    }),
-    next: { revalidate: 60 },
-  });
-
-  if (!res.ok) return null;
-
-  const json = await res.json();
-  if (json.errors) return null;
-
-  return json.data.post;
-}
-
-
-
-
 export default async function BlogPostPage({ params }) {
   const slug = params?.slug;
 
-  const post = await fetchPostBySlug(slug);
-
-  console.log("Slug recibido:", slug);
-  console.log("Post recibido:", post);
-
-  if (!post) {
-    return <p>Artículo no encontrado</p>;
+    if (!slug) {
+    return {
+      title: "Artículo no encontrado POST",
+      robots: { index: false, follow: false },
+    };
   }
+
+  const post = await getPostSEO(slug);
+  if (!post) return { title: "Artículo no encontrado POST", robots: { index: false, follow: false } };
 
   return (
     <article>
